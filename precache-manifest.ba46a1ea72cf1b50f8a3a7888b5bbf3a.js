c0d2c80cfd17e6a2bb61ad741d756f9b12d9b5e2
ooakind <gywls0724@naver.com>
2020-03-14 22:09:29 +0900

    Updates

---
+++ precache-manifest.ba46a1ea72cf1b50f8a3a7888b5bbf3a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f36058028361cf136e2c7833a89164f6",
+    "url": "/colorlovespitch/index.html"
+  },
+  {
+    "revision": "beba09ea15293d708af6",
+    "url": "/colorlovespitch/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "7c683dc58a7ed9697251",
+    "url": "/colorlovespitch/static/js/2.442e38d7.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/colorlovespitch/static/js/2.442e38d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "beba09ea15293d708af6",
+    "url": "/colorlovespitch/static/js/main.4b9e43a4.chunk.js"
+  },
+  {
+    "revision": "e22366126c704bb129e1",
+    "url": "/colorlovespitch/static/js/runtime-main.7a0fa894.js"
+  }
+]);
